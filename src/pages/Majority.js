27d01bfd07c1520feb595e933c907bb39b8df618
Arnob-sen <106549897+Arnob-sen@users.noreply.github.com>
2024-01-01 03:09:03 +0600

    find by search

--- src/pages/Majority.js
+++ src/pages/Majority.js
@@ -1,5 +1,7 @@
-
 import React, { useState } from 'react';
+
+// Global object to store all polls
+const polls = {};
 
 const Option = ({ option, voteCount, onVote }) => (
   <div>
@@ -18,41 +20,47 @@ const Poll = ({ pollId, title, options, votes, onVote }) => (
 );
 
 const Majority = () => {
-  const [poll, setPoll] = useState(null);
+  const [pollId, setPollId] = useState(null);
   const [newOption, setNewOption] = useState('');
   const [title, setTitle] = useState('');
+  const [searchId, setSearchId] = useState('');
 
   const createPoll = () => {
-    if (title !== '') {
-      setPoll({ id: Math.floor(Math.random() * 10000), title, options: [], votes: [] });
-      setTitle('');
-    }
+    const id = Math.floor(Math.random() * 10000);
+    setPollId(id);
+    polls[id] = { title, options: [], votes: [] };
+    setTitle('');
   };
 
   const addOptionToPoll = () => {
     if (newOption !== '') {
-      setPoll({
-        ...poll,
-        options: [...poll.options, newOption],
-        votes: [...poll.votes, 0],
-      });
+      polls[pollId].options.push(newOption);
+      polls[pollId].votes.push(0);
       setNewOption('');
     }
   };
 
   const vote = (optionIndex) => {
-    const newVotes = [...poll.votes];
-    newVotes[optionIndex]++;
-    setPoll({ ...poll, votes: newVotes });
+    polls[pollId].votes[optionIndex]++;
+  };
+
+  const searchPoll = () => {
+    if (polls[searchId]) {
+      setPollId(searchId);
+    } else {
+      alert('Invalid poll ID');
+    }
   };
 
   return (
     <div className="p-4">
+      <input type="text" value={searchId} onChange={(e) => setSearchId(e.target.value)} placeholder="Search poll by ID" />
+      <button onClick={searchPoll} className="px-4 py-2 bg-blue-500 text-white rounded-lg mb-4">Search</button>
       <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Poll title" />
       <button onClick={createPoll} className="px-4 py-2 bg-blue-500 text-white rounded-lg mb-4">Create Poll</button>
-      {poll && (
+      {pollId && (
         <div>
-          <Poll pollId={poll.id} title={poll.title} options={poll.options} votes={poll.votes} onVote={vote} />
+          <Poll pollId={pollId} title={polls[pollId].title} options={polls[pollId].options} votes={polls[pollId].votes} onVote={vote} />
           <input type="text" value={newOption} onChange={(e) => setNewOption(e.target.value)} placeholder="Add option" />
           <button onClick={addOptionToPoll} className="px-4 py-2 bg-blue-500 text-white rounded-lg mb-4">Add Option</button>
         </div>
