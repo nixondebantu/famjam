d9fac8e839335122021c69a6a963e5499aa59a26
Md. Shaeakh Ahmed Chowdhury <shaeakh12@gmail.com>
2023-12-31 14:13:44 +0600

    basic spin wheel added

--- src/pages/JustAsk.js
+++ src/pages/JustAsk.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./JustAsk.css";
+import WheelComponent from "react-wheel-of-prizes";
 
 export default function JustAsk() {
+  const segments = [
+    "Dad",
+    "Mom",
+    "John ",
+    "Nic ",
+    "jack ",
+    "Anny "
+  ];
+
+  const segColors = ["black","#60BA97","black","#60BA97","black","#60BA97",];
+
+  const [winnerMessage, setWinnerMessage] = useState("");
+
+  const onFinished = (winner) => {
+    setWinnerMessage(`It's ${winner}'s turn! :)`);
+  };
+
   return (
-    <>
-        <h1> Welcome to Just Ask </h1>
-    </>
+    <div className="App">
+      <h1>Spin the box! Who knows, it might be you!</h1>
+      <div>
+        <WheelComponent
+          segments={segments}
+          segColors={segColors}
+          onFinished={onFinished}
+          primaryColor="black"
+          contrastColor="white"
+          buttonText="Spin"
+          isOnlyOnce={false}
+          size={250}
+          upDuration={200}
+          downDuration={800}
+          fontFamily="Arial"
+        />
+      </div>
+      <p>{winnerMessage}</p>
+    </div>
   );
 }
