4b5280e6ad7198d663e32c96b1eca32d120ca58c
Nixon Deb Antu <nixondebantu@gmail.com>
2024-01-01 12:16:19 +0600

    JustAsk Dynamic Name Input

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import FamjamLogo from '../img/logo.png';
 
 export default function Navbar() {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -11,7 +12,7 @@ export default function Navbar() {
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+          <img src={FamjamLogo} className="h-8" alt="Famjam Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">FamJam</span>
         </a>
         <button
--- src/pages/JustAsk.js
+++ src/pages/JustAsk.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
-import "./JustAsk.css";
 import WheelComponent from "react-wheel-of-prizes";
+import "./JustAsk.css";
 
 export default function JustAsk() {
-  const segments = [
+  const [segments, setSegments] = useState([
     "Dad",
     "Mom",
-    "John ",
-    "Nic ",
-    "jack ",
-    "Anny "
-  ];
+    "John",
+    "Nic",
+    "Jack",
+    "Anny",
+  ]);
 
-  const segColors = ["black","#60BA97","black","#60BA97","black","#60BA97",];
+  const segColors = ["black", "#60BA97", "black", "#60BA97", "black", "#60BA97"];
 
   const [winnerMessage, setWinnerMessage] = useState("");
 
@@ -20,11 +20,22 @@ export default function JustAsk() {
     setWinnerMessage(`It's ${winner}'s turn! :)`);
   };
 
+  const handleInputChange = (event) => {
+    // Update the segments state with the input values
+    const newSegments = event.target.value.split(",");
+    setSegments(newSegments.map((segment) => segment.trim()));
+  };
+
   return (
     <div className="App">
       <h1>Spin the box! Who knows, it might be you!</h1>
       <div>
+        <label>
+          Enter segments (comma-separated):
+          <input type="text" value={segments.join(",")} onChange={handleInputChange} />
+        </label>
         <WheelComponent
+          key={segments.join()} // Use key prop to force update when segments change
           segments={segments}
           segColors={segColors}
           onFinished={onFinished}
